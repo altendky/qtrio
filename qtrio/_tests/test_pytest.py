2210b4124492a8ea81830134133a7581297cac1a
Kyle Altendorf <sda@fstab.net>
2020-06-20 16:50:52 -0400

    Test a hosted failure

--- qtrio/_tests/test_pytest.py
+++ qtrio/_tests/test_pytest.py
@@ -5,7 +5,7 @@ def test_wait_signal_returns_the_value(testdir):
     import qtrio
 
     @qtrio.host
-    async def test_times_out(request):
+    async def test(request):
         await trio.sleep(10)
     """
     testdir.makepyfile(test_file)
@@ -16,3 +16,19 @@ def test_wait_signal_returns_the_value(testdir):
 
 # TODO: test that the timeout case doesn't leave trio active...  like
 #       it was doing five minutes ago.
+
+
+def test_hosted_assertion_failure_fails(testdir):
+    """QTrio hosted test which fails an assertion fails the test."""
+
+    test_file = r"""
+    import qtrio
+
+    @qtrio.host
+    async def test(request):
+        assert False
+    """
+    testdir.makepyfile(test_file)
+
+    result = testdir.runpytest_subprocess(timeout=10)
+    result.assert_outcomes(failed=1)
