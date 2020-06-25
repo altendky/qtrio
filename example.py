17f0fad1640e012dd5ab151d63fe6b7c26a368d0
Kyle Altendorf <sda@fstab.net>
2020-06-24 21:53:27 -0400

    Window class for the example

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+import attr
 import qtrio
 import qtrio._qt
 import trio
@@ -5,42 +6,65 @@ import trio
 from qtpy import QtWidgets
 
 
+@attr.s(auto_attribs=True)
+class Window:
+    widget: QtWidgets.QWidget
+    increment: QtWidgets.QPushButton
+    decrement: QtWidgets.QPushButton
+    label: QtWidgets.QLabel
+    layout: QtWidgets.QHBoxLayout
+    count: int = 0
+
+    @classmethod
+    def build(cls, parent=None):
+        self = cls(
+            widget = QtWidgets.QWidget(),
+            layout = QtWidgets.QHBoxLayout(),
+            increment = QtWidgets.QPushButton(),
+            decrement = QtWidgets.QPushButton(),
+            label = QtWidgets.QLabel(),
+        )
+
+        self.widget.setLayout(self.layout)
+
+        self.increment.setText("+")
+        self.decrement.setText("-")
+
+        self.label.setText(str(self.count))
+
+        self.layout.addWidget(self.decrement)
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.increment)
+
+        return self
+
+    def increment_count(self):
+        self.count += 1
+        self.label.setText(str(self.count))
+
+    def decrement_count(self):
+        self.count -= 1
+        self.label.setText(str(self.count))
+
+
 async def main():
     application = QtWidgets.QApplication.instance()
 
     with trio.CancelScope() as cancel_scope:
         with qtrio._qt.connection(signal=application.lastWindowClosed, slot=cancel_scope.cancel):
-            widget = QtWidgets.QWidget()
-            layout = QtWidgets.QHBoxLayout()
-            widget.setLayout(layout)
+            window = Window.build()
 
-            increment = QtWidgets.QPushButton()
-            increment.setText("+")
-
-            decrement = QtWidgets.QPushButton()
-            decrement.setText("-")
-
-            count = 0
-            label = QtWidgets.QLabel()
-            label.setText(str(count))
-
-            layout.addWidget(decrement)
-            layout.addWidget(label)
-            layout.addWidget(increment)
-
-            signals = [decrement.clicked, increment.clicked]
+            signals = [window.decrement.clicked, window.increment.clicked]
 
             async with qtrio.open_emissions_channel(signals=signals) as emissions:
-                widget.show()
+                window.widget.show()
 
                 async with emissions.channel:
                     async for emission in emissions.channel:
-                        if emission.is_from(decrement.clicked):
-                            count -= 1
-                        elif emission.is_from(increment.clicked):
-                            count += 1
-
-                        label.setText(str(count))
+                        if emission.is_from(window.decrement.clicked):
+                            window.decrement_count()
+                        elif emission.is_from(window.increment.clicked):
+                            window.increment_count()
 
 
 qtrio.run(main)
