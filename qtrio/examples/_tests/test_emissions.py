0bdf738cb605e6861d3e00f143244cd7bf216666
Kyle Altendorf <sda@fstab.net>
2020-06-26 00:08:06 -0400

    please

--- qtrio/examples/_tests/test_emissions.py
+++ qtrio/examples/_tests/test_emissions.py
@@ -1,44 +1,53 @@
-import qtrio
-from qtpy import QtCore
-import trio
-import trio.testing
-
-import qtrio.examples.emissions
+timeout = 10
 
 
-@qtrio.host
-async def test_example(request, qtbot):
-    window = qtrio.examples.emissions.Window.build()
-    qtbot.addWidget(window.widget)
+def test_main(testdir):
+    test_file = r"""
+    import qtrio
+    from qtpy import QtCore
+    import trio
+    import trio.testing
 
-    results = []
+    import qtrio.examples.emissions
 
-    async def user():
-        await emissions.channel.receive()
+    @qtrio.host
+    async def test_example(request, qtbot):
+        window = qtrio.examples.emissions.Window.build()
+        qtbot.addWidget(window.widget)
 
-        buttons = [
-            window.increment,
-            window.increment,
-            window.increment,
-            window.decrement,
-            window.decrement,
-            window.decrement,
-            window.decrement,
-        ]
-        for button in buttons:
-            qtbot.mouseClick(button, QtCore.Qt.LeftButton)
-            await trio.testing.wait_all_tasks_blocked(cushion=0.01)
-            results.append(window.label.text())
-            await trio.testing.wait_all_tasks_blocked(cushion=0.01)
+        results = []
 
-        await trio.sleep(0.2)
-        window.widget.close()
+        async def user():
+            await emissions.channel.receive()
 
-    async with trio.open_nursery() as nursery:
-        async with qtrio.open_emissions_channel(signals=[window.shown]) as emissions:
-            async with emissions.channel:
-                nursery.start_soon(user)
+            buttons = [
+                window.increment,
+                window.increment,
+                window.increment,
+                window.decrement,
+                window.decrement,
+                window.decrement,
+                window.decrement,
+            ]
+            for button in buttons:
+                qtbot.mouseClick(button, QtCore.Qt.LeftButton)
+                await trio.testing.wait_all_tasks_blocked(cushion=0.01)
+                results.append(window.label.text())
+                await trio.testing.wait_all_tasks_blocked(cushion=0.01)
 
-                await qtrio.examples.emissions.main(window=window)
+            await trio.sleep(1)
+            window.widget.close()
 
-    assert results == ["1", "2", "3", "2", "1", "0", "-1"]
+        async with trio.open_nursery() as nursery:
+            async with qtrio.open_emissions_channel(signals=[window.shown]) as emissions:
+                async with emissions.channel:
+                    nursery.start_soon(user)
+
+                    await qtrio.examples.emissions.main(window=window)
+
+        assert results == ["1", "2", "3", "2", "1", "0", "-1"]
+    """
+    testdir.makepyfile(test_file)
+
+    result = testdir.runpytest_subprocess("--capture=no", timeout=timeout)
+    result.assert_outcomes(passed=1)
