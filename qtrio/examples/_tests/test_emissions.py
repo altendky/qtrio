ad269bd5cffc33d72be14768a9dfed346c2b4574
Kyle Altendorf <sda@fstab.net>
2020-06-25 23:43:33 -0400

    0.2

--- qtrio/examples/_tests/test_emissions.py
+++ qtrio/examples/_tests/test_emissions.py
@@ -92,7 +92,7 @@ def test_94(testdir):
                 results.append(window.label.text())
                 await trio.testing.wait_all_tasks_blocked(cushion=0.01)
 
-            await trio.sleep(1)
+            await trio.sleep(0.2)
             window.widget.close()
 
         async with trio.open_nursery() as nursery:
