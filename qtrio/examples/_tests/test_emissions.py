e86e5cb7b90811a7487a0976c77a261d9a8d52e4
Kyle Altendorf <sda@fstab.net>
2020-06-26 18:19:04 -0400

    put it back

--- qtrio/examples/_tests/test_emissions.py
+++ qtrio/examples/_tests/test_emissions.py
@@ -7,17 +7,52 @@ timeout = 10
 @pytest.mark.xfail(strict=False)
 def test_main_first(testdir):
     test_file = r"""
-    from qtpy import QtWidgets
-    import qtrio
+    import faulthandler
 
+    import qtrio
+    from qtpy import QtCore
+    import trio
+    import trio.testing
+
+    import qtrio.examples.emissions
     @qtrio.host
     async def test_example(request, qtbot):
-        widget = QtWidgets.QWidget()
-        qtbot.addWidget(widget)
-        
-        widget.show()
-        await trio.sleep(2)
-        widget.close()
+        faulthandler.dump_traceback_later(2.5)
+        window = qtrio.examples.emissions.Window.build()
+        qtbot.addWidget(window.widget)
+
+        results = []
+
+        async def user():
+            await emissions.channel.receive()
+
+            buttons = [
+                window.increment,
+                window.increment,
+                window.increment,
+                window.decrement,
+                window.decrement,
+                window.decrement,
+                window.decrement,
+            ]
+            for button in buttons:
+                qtbot.mouseClick(button, QtCore.Qt.LeftButton)
+                await trio.testing.wait_all_tasks_blocked(cushion=0.01)
+                results.append(window.label.text())
+                await trio.testing.wait_all_tasks_blocked(cushion=0.01)
+
+            window.widget.close()
+
+        async with trio.open_nursery() as nursery:
+            async with qtrio.open_emissions_channel(
+                signals=[window.widget.shown],
+            ) as emissions:
+                async with emissions.channel:
+                    nursery.start_soon(user)
+
+                    await qtrio.examples.emissions.main(window=window)
+
+        assert results == ["1", "2", "3", "2", "1", "0", "-1"]
     """
     testdir.makepyfile(test_file)
 
