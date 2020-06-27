8f74555febbe9fc815a8d6a8df266fea980c9303
Kyle Altendorf <sda@fstab.net>
2020-06-27 08:42:28 -0400

    choco

--- ci.sh
+++ ci.sh
@@ -25,7 +25,7 @@ function curl-harder() {
 # We have a Python environment!
 ################################################################
 
-python -c "import sys, struct, ssl; print('#' * 70); print('python:', sys.version); print('version_info:', sys.version_info); print('bits:', struct.calcsize('P') * 8); print('openssl:', ssl.OPENSSL_VERSION, ssl.OPENSSL_VERSION_INFO); print('#' * 70)"
+python -c "import sys, struct, ssl; print('#' * 70); print('python:', sys.executable); print('python:', sys.version); print('version_info:', sys.version_info); print('bits:', struct.calcsize('P') * 8); print('openssl:', ssl.OPENSSL_VERSION, ssl.OPENSSL_VERSION_INFO); print('#' * 70)"
 
 python -m pip install -U pip setuptools wheel
 python -m pip --version
