98805eba6f17399aa29a67acf3460807aa22cf0c
Kyle Altendorf <sda@fstab.net>
2020-06-28 12:42:36 -0400

    Fill out pytest command

--- ci.ps1
+++ ci.ps1
@@ -6,3 +6,17 @@ venv/scripts/pip install ".$Env:INSTALL_EXTRAS"
 mkdir empty
 cd empty
 ../venv/scripts/pytest qtrio --pyargs
+$Env:INSTALLDIR=$(python -c "import os, ${PACKAGE_NAME}; print(os.path.dirname(${PACKAGE_NAME}.__file__))")
+cp ../setup.cfg $Env:INSTALLDIR
+if ()pytest -W error -r a --junitxml=../test-results.xml $Env:INSTALLDIR --cov="$Env:INSTALLDIR" --cov-config=../.coveragerc --verbose --capture=no --no-qt-log){
+    $Env:PASSED = 0
+} else {
+    $Env:PASSED = 1
+}
+
+# https://github.com/codecov/codecov-exe#introduction
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/codecov/codecov-exe/releases/download/1.12.0/codecov-win7-x64.zip", (Join-Path $pwd "Codecov.zip"))
+Expand-Archive .\Codecov.zip -DestinationPath .
+.\Codecov\codecov.exe -n "$Env:JOB_NAME"
+
+exit $Env:PASSED
