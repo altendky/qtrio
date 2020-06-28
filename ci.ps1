50321aeb2802913ed5c3132cd684256b410bdbbb
Kyle Altendorf <sda@fstab.net>
2020-06-28 08:52:11 -0400

    dddddddddddddddddddddddddddddddddddddddd

--- ci.ps1
+++ ci.ps1
@@ -2,7 +2,7 @@ Set-PSDebug -Trace 1
 python -c "import os; import sys; print(os.getcwd()); print(sys.executable); print(sys.version_info)"
 python -m venv venv
 venv/scripts/python -m pip install --upgrade pip setuptools wheel
-venv/scripts/pip install .$Env:INSTALL_EXTRAS
+venv/scripts/pip install ".$Env:INSTALL_EXTRAS"
 mkdir empty
 cd empty
 ../venv/scripts/pytest qtrio --pyargs
