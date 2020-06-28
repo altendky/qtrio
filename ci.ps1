b349215536999956e2334776aa17f6fe788ee2ab
Kyle Altendorf <sda@fstab.net>
2020-06-28 08:05:03 -0400

    dddddddddddddddddddddddddddddddddd

---
+++ ci.ps1
@@ -0,0 +1,10 @@
+py --help
+py --list
+python -c "import os; import sys; print(os.getcwd()); print(sys.executable); print(sys.version_info)"
+cd c:/qtrio
+python -m venv venv
+venv/scripts/python -m pip install --upgrade pip setuptools wheel
+venv/scripts/pip install .${INSTALL_EXTRAS}
+mkdir empty
+cd empty
+../venv/scripts/pytest qtrio --pyargs
