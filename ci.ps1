5858649ab956ff5cdde44b4569b231159c4a69a6
Kyle Altendorf <sda@fstab.net>
2020-06-28 09:51:23 -0400

    Revert "dddddddddddddddddddddddddddddddddddddddddddd"
    This reverts commit 717b2c9c06b3f48c61e9b297cda6eaa6700ed389.

--- ci.ps1
+++ ci.ps1
@@ -1,5 +1,4 @@
 Set-PSDebug -Trace 1
-failoutherebecausethisisinvalidandmakesuretheerrorpropogatestofailci
 python -c "import os; import sys; print(os.getcwd()); print(sys.executable); print(sys.version_info)"
 python -m venv venv
 venv/scripts/python -m pip install --upgrade pip setuptools wheel
