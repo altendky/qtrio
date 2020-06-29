75612cc37fc99a9875f1fb533f6509f435bdf73e
Kyle Altendorf <sda@fstab.net>
2020-06-29 19:14:32 -0400

    docstring format tweak

--- qtrio/_tests/test_cli.py
+++ qtrio/_tests/test_cli.py
@@ -5,8 +5,9 @@ import sysconfig
 
 
 def test_emissions_help():
-    """The console script provides --help for the emissions example."""
-
+    """The console script doesn't fail when asked for --help for the emissions
+    example.
+    """
     scripts = pathlib.Path(sysconfig.get_path("scripts"))
     subprocess.run(
         [os.fspath(scripts / "qtrio"), "examples", "emissions", "--help"], check=True
