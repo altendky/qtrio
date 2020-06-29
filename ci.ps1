5da7760877b6a39107f58ad16ef216ff46877676
Kyle Altendorf <sda@fstab.net>
2020-06-29 17:57:48 -0400

    cleanup with QT_QPA_PLATFORM=offscreen

--- ci.ps1
+++
@@ -1,8 +0,0 @@
-Set-PSDebug -Trace 1
-python -c "import os; import sys; print(os.getcwd()); print(sys.executable); print(sys.version_info)"
-python -m venv venv
-venv/scripts/python -m pip install --upgrade pip setuptools wheel
-venv/scripts/pip install ".$Env:INSTALL_EXTRAS"
-mkdir empty
-cd empty
-../venv/scripts/pytest qtrio --pyargs
