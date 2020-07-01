db52aba8703a8533f90d015865a13a50f485b5de
Kyle Altendorf <sda@fstab.net>
2020-06-30 22:39:04 -0400

    recover cli entry

--- qtrio/_cli.py
+++ qtrio/_cli.py
@@ -16,6 +16,25 @@ def examples():
 
 
 @examples.command()
+@click.option("--url", help="The URL to download.")
+@click.option(
+    "--destination",
+    help="The file path to save to.",
+    type=click.Path(dir_okay=False, writable=True, resolve_path=True, allow_dash=True),
+)
+@click.option(
+    "--fps",
+    default=60,
+    help="Frames per second for progress updates.",
+    type=click.IntRange(min=1),
+)
+def download(url, destination, fps):  # pragma: no cover
+    import qtrio.examples.download
+
+    qtrio.run(qtrio.examples.download.main, url, destination, fps)
+
+
+@examples.command()
 def emissions():  # pragma: no cover
     """A simple demonstration of iterating over signal emissions."""
 
