e3bcb0f66004c423823b80cbdb27d96f917cd86a
Kyle Altendorf <sda@fstab.net>
2020-07-08 20:20:30 -0400

    Use PyQt5-stubs PR for now

--- ci.sh
+++ ci.sh
@@ -41,6 +41,7 @@ python -m pip install ${INSTALL_ARTIFACT}${INSTALL_EXTRAS}
 
 if [ "$CHECK_TYPE_HINTS" = "1" ]; then
     pip install https://github.com/altendky/qtpy/archive/mypy.zip
+    pip install https://github.com/altendky/PyQt5-stubs/archive/pyqtBoundSignal.signal.zip
 end
 
 python -m pip list
