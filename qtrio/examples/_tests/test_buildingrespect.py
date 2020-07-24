7faf1bbe494ecf1b2dcb4c2652e121f884341bb2
Kyle Altendorf <sda@fstab.net>
2020-07-24 10:25:26 -0400

    just drop the async etc

--- qtrio/examples/_tests/test_buildingrespect.py
+++ qtrio/examples/_tests/test_buildingrespect.py
@@ -1,28 +1,16 @@
 import time
 
-import qtrio
-from qtpy import QtCore
 from qtpy import QtWidgets
-import trio
-import trio.testing
-
-import qtrio.examples.buildingrespect
 
 
 clock = time.monotonic
 
 
-@qtrio.host
-async def test_example(request, qtbot):
+def test_example(qtbot):
     start = clock()
 
     def delta():
         return f'{clock() - start:0.3f}'
-
-    async def user():
-        print('+++++', 'test_example user() before sleep', delta())
-        await trio.sleep(0)
-        print('+++++', 'test_example user() after sleep', delta())
 
     def show_button(message):
         print(f'+++++ {message} - before button creation', delta())
@@ -33,16 +21,4 @@ async def test_example(request, qtbot):
         button.show()
         print(f'+++++ {message} - after button.show()', delta())
 
-    print('+++++', 'test_example before nursery', delta())
-
-    try:
-        show_button('before nursery')
-
-        async with trio.open_nursery() as nursery:
-            show_button('in nursery')
-
-            nursery.start_soon(user)
-
-            show_button('after start_soon')
-    finally:
-        print('+++++', 'test_example finally', delta())
+    show_button('before nursery')
