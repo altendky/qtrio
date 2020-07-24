5daeb100cbb1cacfe633dc15f41eee38adac2d81
Kyle Altendorf <sda@fstab.net>
2020-07-24 08:41:23 -0400

    Revert "preshow_fixture for all"
    This reverts commit 5a3ea49b3ff7e14d73db890045dd1efb61ee6078.

--- qtrio/_pytest.py
+++ qtrio/_pytest.py
@@ -1,37 +1,15 @@
 """Tools to help working with QTrio in pytest."""
 
 import functools
-import time
 import typing
-
 
 import outcome
 import pytest
-from qtpy import QtWidgets
 
 import qtrio
 
 
 timeout = 3
-
-
-@pytest.fixture(name="qtrio_preshow_workaround", scope="session", autouse=True)
-def preshow_fixture(qapp):
-    widget = QtWidgets.QWidget()
-
-    clock = time.monotonic
-    start = clock()
-    widget.show()
-    end = clock()
-
-    show_time = end - start
-
-    print(
-        "FYI, the qtrio_preshow_workaround fixture widget show time was"
-        + f" {show_time:0.3f} seconds..."
-    )
-
-    yield
 
 
 def host(test_function: typing.Callable[..., typing.Awaitable[None]]):
