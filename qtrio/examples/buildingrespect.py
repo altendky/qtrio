7a9583c06172e674dd3bfe335ca82e86a331c158
Kyle Altendorf <sda@fstab.net>
2020-07-24 12:11:43 -0400

    instrument the script itself

--- qtrio/examples/buildingrespect.py
+++ qtrio/examples/buildingrespect.py
@@ -1,17 +1,28 @@
+import time
 import typing
 
 import qtrio
 from qtpy import QtWidgets
 
 
+clock = time.monotonic
+
+
 async def main(button: typing.Optional[QtWidgets.QPushButton] = None):
+    start = clock()
+
+    def delta():
+        return f'{clock() - start:0.3f}'
+
     if button is None:  # pragma: no cover
         button = QtWidgets.QPushButton()
 
     button.setText("Exit")
 
     async with qtrio.enter_emissions_channel(signals=[button.clicked]) as emissions:
+        print(f'+++++ before button.show()', delta())
         button.show()
+        print(f'+++++ after button.show()', delta())
 
         await emissions.channel.receive()
 
