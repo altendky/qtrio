e2b0e6334dc37421e843a06d5c9538e98c12e3bd
Kyle Altendorf <sda@fstab.net>
2020-07-24 16:43:25 -0400

    Preshow QPushButton instead

--- qtrio/conftest.py
+++ qtrio/conftest.py
@@ -1,6 +1,5 @@
 import inspect
 import sys
-import time
 
 import pytest
 from qtpy import QtWidgets
@@ -10,19 +9,10 @@ pytest_plugins = "pytester"
 
 @pytest.fixture(name="qtrio_preshow_workaround", scope="session", autouse=True)
 def preshow_fixture(qapp):
-    widget = QtWidgets.QWidget()
+    widget = QtWidgets.QPushButton()
 
-    clock = time.monotonic
-    start = clock()
     widget.show()
-    end = clock()
-
-    show_time = end - start
-
-    print(
-        "FYI, the qtrio_preshow_workaround fixture widget show time was"
-        + f" {show_time:0.3f} seconds..."
-    )
+    widget.hide()
 
 
 @pytest.fixture(name="preshow_testdir")
