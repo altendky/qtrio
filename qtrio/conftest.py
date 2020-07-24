3ba58aa5b071167fc10cbb4dd291af09d0ac6da8
Kyle Altendorf <sda@fstab.net>
2020-07-23 23:50:11 -0400

    Add qtrio_preshow_workaround fixture to use up the first slow .show()

--- qtrio/conftest.py
+++ qtrio/conftest.py
@@ -1,1 +1,24 @@
+import time
+
+import pytest
+from qtpy import QtWidgets
+
 pytest_plugins = "pytester"
+
+
+@pytest.fixture(name='qtrio_preshow_workaround', scope='session', autouse=True)
+def preshow_fixture(qapp):
+    clock = time.monotonic
+
+    def delta():
+        return f'{clock() - start:0.3f}'
+
+    widget = QtWidgets.QWidget()
+
+    start = clock()
+    widget.show()
+    end = clock()
+
+    show_time = end - start
+
+    print(f'FYI, the qtrio_preshow_workaround fixture widget show time was {show_time:0.3f} seconds...')
