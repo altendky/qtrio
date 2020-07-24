b779eb987444ec322753065dbbdb17604917b0d0
Kyle Altendorf <sda@fstab.net>
2020-07-23 23:54:42 -0400

    cleanup

--- qtrio/conftest.py
+++ qtrio/conftest.py
@@ -6,19 +6,18 @@ from qtpy import QtWidgets
 pytest_plugins = "pytester"
 
 
-@pytest.fixture(name='qtrio_preshow_workaround', scope='session', autouse=True)
+@pytest.fixture(name="qtrio_preshow_workaround", scope="session", autouse=True)
 def preshow_fixture(qapp):
-    clock = time.monotonic
-
-    def delta():
-        return f'{clock() - start:0.3f}'
-
     widget = QtWidgets.QWidget()
 
+    clock = time.monotonic
     start = clock()
     widget.show()
     end = clock()
 
     show_time = end - start
 
-    print(f'FYI, the qtrio_preshow_workaround fixture widget show time was {show_time:0.3f} seconds...')
+    print(
+        "FYI, the qtrio_preshow_workaround fixture widget show time was"
+        + f" {show_time:0.3f} seconds..."
+    )
