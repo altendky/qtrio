e7a10443229eac80980ee71874801a5f93fc7764
Kyle Altendorf <sda@fstab.net>
2020-07-23 21:08:54 -0400

    no subprocess

--- qtrio/examples/_tests/test_buildingrespect.py
+++ qtrio/examples/_tests/test_buildingrespect.py
@@ -1,46 +1,36 @@
-timeout = 10
+import qtrio
+from qtpy import QtCore
+from qtpy import QtWidgets
+import trio
+import trio.testing
+
+import qtrio.examples.buildingrespect
 
 
-def test_main(testdir):
-    test_file = r"""
-    import qtrio
-    from qtpy import QtCore
-    from qtpy import QtWidgets
-    import trio
-    import trio.testing
+class SignaledButton(QtWidgets.QPushButton):
+    shown = QtCore.Signal()
 
-    import qtrio.examples.buildingrespect
+    def showEvent(self, event):
+        super().showEvent(event)
+        if event.isAccepted():
+            self.shown.emit()
 
 
-    class SignaledButton(QtWidgets.QPushButton):
-        shown = QtCore.Signal()
+@qtrio.host
+async def test_example(request, qtbot):
+    button = SignaledButton()
+    qtbot.addWidget(button)
 
-        def showEvent(self, event):
-            super().showEvent(event)
-            if event.isAccepted():
-                self.shown.emit()
+    async def user():
+        await emissions.channel.receive()
 
-
-    @qtrio.host
-    async def test_example(request, qtbot):
-        button = SignaledButton()
-        qtbot.addWidget(button)
-
-        async def user():
-            await emissions.channel.receive()
-
-            button.click()
+        button.click()
 
 
-        async with trio.open_nursery() as nursery:
-            async with qtrio.enter_emissions_channel(
-                signals=[button.shown],
-            ) as emissions:
-                nursery.start_soon(user)
+    async with trio.open_nursery() as nursery:
+        async with qtrio.enter_emissions_channel(
+            signals=[button.shown],
+        ) as emissions:
+            nursery.start_soon(user)
 
-                await qtrio.examples.buildingrespect.main(button=button)
-    """
-    testdir.makepyfile(test_file)
-
-    result = testdir.runpytest_subprocess("--capture=no", timeout=timeout)
-    result.assert_outcomes(passed=1)
+            await qtrio.examples.buildingrespect.main(button=button)
