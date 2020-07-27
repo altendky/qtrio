4f938417e11b016ed214266bdb499b8577833706
Kyle Altendorf <sda@fstab.net>
2020-07-27 17:10:04 -0400

    longhand

--- setup.py
+++ setup.py
@@ -43,8 +43,7 @@ setup(
     extras_require={
         "checks": ["black", "flake8", "mypy", "towncrier>=19.9.0rc1"],
         "docs": [
-            # != 3.1.2 for https://github.com/sphinx-doc/sphinx/issues/8008
-            "sphinx >= 1.7.0, != 3.1.2",
+            "sphinx >= 1.7.0",
             "sphinx-autodoc-typehints",
             "sphinx-qt-documentation>=0.3",
             "sphinx_rtd_theme",
