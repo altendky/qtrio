afa9a072730fd0766be74a58ef4bdcd240f19646
Kyle Altendorf <sda@fstab.net>
2020-07-30 14:56:03 -0400

    sitecustomize.py

--- ci.sh
+++ ci.sh
@@ -67,7 +67,7 @@ else
     INSTALLDIR=$(python -c "import os, ${PACKAGE_NAME}; print(os.path.dirname(${PACKAGE_NAME}.__file__))")
     cp ../setup.cfg $INSTALLDIR
     mkdir pp
-    echo 'import coverage; coverage.process_startup()' > pp/coverage.pth
+    echo 'import coverage; coverage.process_startup()' > pp/sitecustomize.py
     export COVERAGE_PROCESS_START=1
     export PYTHONPATH=${PYTHONPATH}:pp
     if coverage run --rcfile ../.coveragerc --source ${PACKAGE_NAME} -m pytest -W error -r a --junitxml=../test-results.xml --verbose --capture=no --no-qt-log --pyargs ${PACKAGE_NAME}; then
