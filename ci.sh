9ab784495b09ce82d38dc5a76ffe81721dd62ff2
Kyle Altendorf <sda@fstab.net>
2020-07-30 14:46:22 -0400

    hmm

--- ci.sh
+++ ci.sh
@@ -66,6 +66,10 @@ else
 
     INSTALLDIR=$(python -c "import os, ${PACKAGE_NAME}; print(os.path.dirname(${PACKAGE_NAME}.__file__))")
     cp ../setup.cfg $INSTALLDIR
+    mkdir pp
+    echo 'import coverage; coverage.process_startup()' > pp/coverage.pth
+    export COVERAGE_PROCESS_START=1
+    export PYTHONPATH=${PYTHONPATH}:pp
     if coverage run --rcfile ../.coveragerc --source ${PACKAGE_NAME} -m pytest -W error -r a --junitxml=../test-results.xml --verbose --capture=no --no-qt-log --pyargs ${PACKAGE_NAME}; then
         PASSED=true
     else
