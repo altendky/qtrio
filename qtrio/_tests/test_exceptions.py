f85b463be73db185fde0fb8c87c9145e527d5b98
Kyle Altendorf <sda@fstab.net>
2020-08-07 21:15:09 -0400

    and also more more

--- qtrio/_tests/test_exceptions.py
+++ qtrio/_tests/test_exceptions.py
@@ -5,5 +5,9 @@ def test_return_code_error_equality():
     assert qtrio.ReturnCodeError(1) == qtrio.ReturnCodeError(1)
 
 
-def test_return_code_error_inequality():
+def test_return_code_error_inequality_by_value():
     assert qtrio.ReturnCodeError(1) != qtrio.ReturnCodeError(2)
+
+
+def test_return_code_error_inequality_by_type():
+    assert qtrio.ReturnCodeError(1) != Exception()
