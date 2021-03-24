9d6035f8781b930221c208764f91049ffb869525
Kyle Altendorf <sda@fstab.net>
2021-03-24 16:30:10 -0400

    make download example directly runnable

--- qtrio/examples/download.py
+++ qtrio/examples/download.py
@@ -22,6 +22,8 @@ import qtrio.dialogs
 # Default is 4096
 httpcore._async.http11.AsyncHTTP11Connection.READ_NUM_BYTES = 100_000
 
+default_fps: int = 60
+
 
 def create_title(specific: str) -> str:
     return f"QTrio Download Example - {specific}"
@@ -30,8 +32,8 @@ def create_title(specific: str) -> str:
 @attr.s(auto_attribs=True, frozen=True)
 class Progress:
     downloaded: int
-    total: typing.Optional[int]
     first: bool
+    total: typing.Optional[int] = None
 
 
 @attr.s(auto_attribs=True, eq=False)
@@ -46,9 +48,9 @@ class Downloader:
 
     async def serve(
         self,
-        url: typing.Optional[typing.Union[str, hyperlink.URL]],
-        destination: typing.Optional[typing.Union[str, os.PathLike]],
-        fps: float = 60,
+        url: typing.Optional[typing.Union[str, hyperlink.URL]] = None,
+        destination: typing.Optional[typing.Union[str, os.PathLike]] = None,
+        fps: float = default_fps,
         http_application: typing.Optional[typing.Callable[..., typing.Any]] = None,
         *,
         task_status: trio_typing.TaskStatus[None] = trio.TASK_STATUS_IGNORED,
@@ -108,10 +110,10 @@ class Downloader:
 
 
 async def start_downloader(
-    url: typing.Optional[hyperlink.URL],
-    destination: typing.Optional[trio.Path],
-    fps: float,
-    http_application: typing.Callable[..., typing.Any],
+    url: typing.Optional[hyperlink.URL] = None,
+    destination: typing.Optional[trio.Path] = None,
+    fps: float = default_fps,
+    http_application: typing.Optional[typing.Callable[..., typing.Any]] = None,
     hold_event: typing.Optional[trio.Event] = None,
     *,
     cls: typing.Type[Downloader] = Downloader,
@@ -131,7 +133,7 @@ async def start_downloader(
 
 @attr.s(auto_attribs=True, eq=False)
 class GetDialog:
-    fps: float = 60
+    fps: float = default_fps
     clock: typing.Callable[[], float] = time.monotonic
     http_application: typing.Optional[typing.Callable[..., typing.Any]] = None
 
@@ -218,12 +220,12 @@ class GetDialog:
 async def start_get_dialog(
     url: hyperlink.URL,
     destination: trio.Path,
-    fps: float,
-    http_application: typing.Callable[..., typing.Any],
+    fps: float = default_fps,
+    http_application: typing.Optional[typing.Callable[..., typing.Any]] = None,
     hold_event: typing.Optional[trio.Event] = None,
     *,
     cls: typing.Type[GetDialog] = GetDialog,
-    task_status: trio_typing.TaskStatus["GetDialog"] = trio.TASK_STATUS_IGNORED,
+    task_status: trio_typing.TaskStatus[GetDialog] = trio.TASK_STATUS_IGNORED,
 ) -> None:
     self = cls(fps=fps, http_application=http_application)
 
@@ -276,3 +278,7 @@ async def get(
             if progress.downloaded != downloaded:
                 progress = attr.evolve(progress, downloaded=downloaded)
                 yield progress
+
+
+if __name__ == "__main__":
+    qtrio.run(start_downloader)
