6391990e44f22b78cf8113aa4ae83fa7021dabbd
Kyle Altendorf <sda@fstab.net>
2021-07-06 13:41:26 -0400

    add a test to make sure importing qtrio does not import any qt wrapper

--- qtrio/_tests/test_qtrio.py
+++ qtrio/_tests/test_qtrio.py
@@ -17,3 +17,30 @@ def test_importing_qtrio_does_not_import_qt(testdir):
 
     result = testdir.runpytest_subprocess('-p', 'no:qt')
     result.assert_outcomes(passed=1)
+
+
+def test_importing_qtrio_does_not_import_qt_wrappers(testdir):
+    test_file = r"""
+    import sys
+
+    def wrapper_modules():
+        return [
+            module
+            for module in sys.modules
+            if (
+                module.startswith('PyQt')
+                or module.startswith('PySide')
+            )
+        ]
+
+    def test():
+        assert wrapper_modules() == [], "Qt wrapper modules should not have been imported prior to the test"
+
+        import qtrio
+
+        assert wrapper_modules() == [], "Qt wrapper modules should not have been imported during test"
+    """
+    testdir.makepyfile(test_file)
+
+    result = testdir.runpytest_subprocess('-p', 'no:pytest-qt')
+    result.assert_outcomes(passed=1)
